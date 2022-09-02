c6d60159089f59091d5e876c750154c431a38335
Jeff Felkowski <111080091+jfelkowski@users.noreply.github.com>
2022-09-02 14:48:03 -0400

    completed run and load js lect

--- playground/types.js
+++ playground/types.js
@@ -1,30 +1,49 @@
 /* eslint-disable */
+
+// const name = 'wes';
+//const middle = "topher";
+//const last = `bos`;
+
+//const sentence = "she's so \"cool\"";
+//const sentence2 = `she's so "cool"`;
+
+//const song = `Ohh
+
+//ya
+
+//I like
+//pizza`;
+
+//const hello = 'hello my name is' + name + ". Nice to meet you";
+
+// let hello2 = 'hello my name is ';
+// hello2 = hello2 + name;
+// hello2 = hello2 + 'Nice to meet you';
+
+// const hello = `hello my name is ${name}. Nice to meet you. I am ${1 + 100} years old`;
+
+// const html = `
+//    <div>
+//     <h2>${name}</h2>
+//     <p>${hello}</p>
+//    </div>
+// `;
+
+
+// document.body.innerHTML = html;
+
+//console.log(html);
+
+const age = 100;
 const name = 'wes';
-const middle = "topher";
-const last = `bos`;
 
-const sentence = "she's so \"cool\"";
-const sentence2 = `she's so "cool"`;
+// const a = 10;
+// const b = 20;
 
-const song = `Ohh
+const smarties = 20;
+const kids = 3;
+const eachKidGets = Math.floor(smarties / kids);
+const dadGets = smarties % kids;
+console.log(`Each kid gets ${eachKidGets}`);
 
-ya
-
-I like
-pizza`;
-
-// const hello = 'hello my name is' + name + ". Nice to meet you";
-
-const hello = `hello my name is ${name}. Nice to meet you. I am ${1 + 100} years old`;
-
-const html = `
-    <div>
-     <h2>${name}</h2>
-     <p>${hello}</p>
-    </div>
-`;
-
-
-document.body.innerHTML = html;
-
-console.log(html);
+const price = 10.34;
