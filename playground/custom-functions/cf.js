f6200f4187ec887fb655c4b8980d2d3bd366ed17
Jeff Felkowski <111080091+jfelkowski@users.noreply.github.com>
2022-09-09 09:47:14 -0400

    success run and load js lect

--- playground/custom-functions/cf.js
+++ playground/custom-functions/cf.js
@@ -1,13 +1,30 @@
 // Function Definition
-function calculateBill() {
+function calculateBill(billAmount, taxRate = 0.13, tipRate = 0.15) 
+{
   // this is the function body
   console.log('Running Calculate Bill!!');
-  const total = 100 * 1.13;
+  const total = billAmount + billAmount * taxRate + billAmount * taxRate;
   return total;
 }
 
 // Function Call. or ** RUN **
-const myTotal = calculateBill();
-// console.log(`Your Total is $${myTotal}`);
+const wesTotal = 500;
+const wesTaxRate = 0.3;
+// const myTotal = calculateBill(wesTotal, wesTaxRate);
 
-console.log(`Your total is $${calculateBill()}`);
+function sayHiTo(firstName) {
+  return `Hello ${firstName}`;
+}
+// const greeting = sayHiTo('Wes');
+// console.log(greeting);
+
+function doctorize(name) {
+  return `Dr. ${name}`;
+}
+
+function yell(name = 'Silly Goose') {
+  return `Hey ${name.toUpperCase()}`;
+}
+
+const myBill4 = calculateBill(100, undefined, 0.2);
+console.log(myBill4);
