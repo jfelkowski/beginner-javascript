74c999ee35da41bfdfd3969d188b5c54e48eeb48
Jeff Felkowski <111080091+jfelkowski@users.noreply.github.com>
2022-10-07 15:32:26 -0400

    success run and load js LECT

---
+++ exercises/29 - Events/forms.js
@@ -0,0 +1,35 @@
+const wes = document.querySelector('.wes');
+
+wes.addEventListener('click', function(event) {
+  const shouldChangePage = confirm(
+    'This website might be malicious!, do you wish to proceed?'
+  );
+  if(!shouldChangePage) {
+    event.preventDefault();
+  }
+});
+
+const signupForm = document.querySelector('[name="signup"]');
+
+signupForm.addEventListener('submit', function(event) {
+  event.preventDefault();
+  const name = event.currentTarget.name.value;
+  if (name.includes('chad')) {
+    alert('Sorry Bro');
+    event.preventDefault();
+  }
+});
+
+function logEvent(event) {
+  console.log(event.type);
+  console.log(event.currentTarget.value);
+}
+
+signupForm.name.addEventListener('keyup', logEvent);
+signupForm.name.addEventListener('keydown', logEvent);
+signupForm.name.addEventListener('focus', logEvent);
+signupForm.name.addEventListener('blur', logEvent);
+// 'keyup'
+// 'keydown'
+// 'focus'
+// 'blur'
