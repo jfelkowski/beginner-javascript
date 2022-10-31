6e8f3767df8263220f2619e32b22ac6ab59b6ee9
Jeff Felkowski <111080091+jfelkowski@users.noreply.github.com>
2022-10-31 12:33:01 -0400

    success run and load JS lect

--- exercises/75 - CORS and Recipes/scripts.js
+++ exercises/75 - CORS and Recipes/scripts.js
@@ -1,2 +1,48 @@
+
 // The Recipe Puppy API used in the course is broken
 // Please use this replacement API URL "https://recipes.beginnerjavascript.com/api"
+
+// To use Cors anywhere, you now need to request temporary access.
+// -You can do that here: https://cors-anywhere.herokuapp.com/corsdemo
+
+const baseEndpoint = 'https://recipes.beginnerjavascript.com/api';
+const proxy = 'https://cors-anywhere.herokuapp.com/';
+const $form = document.querySelector('form.search');
+const $recipesGrid = document.querySelector('.recipes');
+
+async function fetchRecipes(query) {
+  // https://cors-anywhere.herokuapp.com/https://recipes.beginnerjavascript.com/api?q=pizza
+  const res = await fetch(`${proxy}${baseEndpoint}?q=${query}`);
+  const data = await res.json();
+  return data;
+}
+
+function displayRecipes(recipes) {
+  console.log('Creating html', recipes);
+  const html = recipes.map(
+    recipe => `<div class="recipe">
+      <h2>${recipe.title}<h2>
+      <p>${recipe.ingredients}</p>
+      ${recipe.thumbnail &&
+      `<img src="${recipe.thumbnail}" alt="${recipe.title}"/>`}
+      <a href="${recipe.href}" target="_blank">View Recipe</a>
+    </div>`
+  );
+  $recipesGrid.innerHTML = html.join('');
+}
+
+async function fetchAndDisplay(query) {
+  $form.submit.disabled = true;
+  const recipes = await fetchRecipes(query);
+  $form.submit.disabled = false;
+  displayRecipes(recipes.results);
+}
+
+async function handleSubmit(event) {
+  event.preventDefault();
+  const el = event.currentTarget;
+  fetchAndDisplay(el.query.value);
+}
+
+$form.addEventListener('submit', handleSubmit);
+fetchAndDisplay('pizza');
